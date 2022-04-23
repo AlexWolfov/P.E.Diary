c18754cd62747270076739408e581657c3442f9c
MkSerdyuk <git_user@mail.ru>
2022-04-23 23:26:24 +0300

    Начато создание формы для создания нового норматива

---
+++ P.E.Diary/Widgets/NewNromativeDialog.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P.E.Diary.Widgets
+{
+    /// <summary>
+    /// Interaction logic for NewNromativeDialog.xaml
+    /// </summary>
+    public partial class NewNromativeDialog : Window
+    {
+        public NewNromativeDialog()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CreateNormative()
+        {
+            Normative normative = new Normative();
+            normative.Name = NameBox.Text;
+            normative.Formula = Formula.Text;
+            if (Types.SelectedItem != null)
+            {
+                normative.Type = Types.SelectedItem.ToString();
+            }
+            else
+            {
+                normative.Type = "Без категории";
+            }
+            SqlReader.CreateNormative(normative);
+            _normativesList.LoadNormatives();
+        }
+
+        public void LoadTypes()
+        {
+            if ((Types != null))
+            {
+                Types.Items.Clear();
+            }
+            Types.Items.AddRange(SqlReader.LoadTypes().ToArray()); //загружаем список типов
+        }
+
+        private void Create_Click(object sender, EventArgs e)
+        {
+            if (NameBox.Text != "" && Formula.Text != "")
+            {
+                CreateNormative();
+                Close();
+            }
+            else
+            {
+                FoolProof.UniversalProtection("Заполните имя и формулу");
+            }
+        }
+
+        private void NewType_Click(object sender, EventArgs e)
+        {
+            NewType newTypeForm = new NewType(this);
+            newTypeForm.Show();
+        }
+
+        private void DeleteType_Click(object sender, EventArgs e)
+        {
+            if (Types.SelectedItem != null)
+            {
+                SqlReader.DeleteType(Types.SelectedItem.ToString());
+                SqlReader.LoadNormatives();
+                LoadTypes();
+            }
+            else
+            {
+                FoolProof.UniversalProtection("Нельзя удалить отсутствующую категорию");
+            }
+        }
+    }
+}
---
+++ P.E.Diary/Widgets/NormativesLeftMenu.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P.E.Diary.Widgets
+{
+    /// <summary>
+    /// Interaction logic for NormativesLeftMenu.xaml
+    /// </summary>
+    public partial class NormativesLeftMenu : UserControl
+    {
+        public NormativesLeftMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+         
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
